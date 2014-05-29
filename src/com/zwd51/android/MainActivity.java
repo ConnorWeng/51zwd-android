e58a764155c520263a9cb059a099c3b5fa02248e
Connor Weng <foamtea30@gmail.com>
2014-05-30 00:19:58 +0800

    Create simple UI let user input taobao item id

--- src/com/zwd51/android/MainActivity.java
+++ src/com/zwd51/android/MainActivity.java
@@ -2,8 +2,16 @@ package com.zwd51.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+    private static final String TAG = "MainActivity";
+    private EditText editTextTaobaoItemId;
+    private Button buttonUpload;
+
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +19,21 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        initView();
+        bindEventListener();
+    }
+
+    private void bindEventListener() {
+        buttonUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "upload button clicked and taobaoItemId is " + editTextTaobaoItemId.getText());
+            }
+        });
+    }
+
+    private void initView() {
+        editTextTaobaoItemId = (EditText) findViewById(R.id.editTextTaobaoItemId);
+        buttonUpload = (Button) findViewById(R.id.buttonUpload);
     }
 }
